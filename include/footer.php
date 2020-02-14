2e0f03b08a67ef54f928aa94271fd88d19a8b3bd
amirhamza05 <sk.amirhamza@gmail.com>
2020-02-14 20:06:08 +0600

    v1.2 (Update Footer)

--- include/footer.php
+++ include/footer.php
@@ -7,7 +7,7 @@
 <div class="col-sm-12" style="padding-top: 80px;">
 </div>
   <div class="container-fluid col-sm-12">
-  <div class="row" style="padding-top: 5px; padding-bottom: 15px; color: #000000;   border: 1px solid #E7ECF1; margin-top: 80px;border-width: 1px 0px 0px 0px; font-weight: bold;">
+  <div class="row" style="padding-top: 5px; padding-bottom: 15px; color: #000000;   border: 1px solid #E7ECF1; margin-top: 30px;border-width: 1px 0px 0px 0px; font-weight: bold;">
     <div class="col-sm-0">
     </div>
     <div class="col-sm-12">
@@ -16,8 +16,8 @@
           <div class="col-sm-12"  style="padding-top: 2px;">
              <p style="font-size:14px; text-align: center; color: #4A525B "><font face="Times New Roman"> 
 
-            &copy; 2020 CoderOJ<br/>
-            Developed By: Sk.Amir Hamza<br/>
+            &copy; 2020 CoderOJ | TechSerm<br/>
+            Judge Developed By: Sk.Amir Hamza<br/>
              Server Time: <a id="server_time"></a>
             
              
