d42f926823551e84a099bb28a3cd9cddd72afcf7
jugal-chanda <jugalchanda7@gmail.com>
2021-03-01 02:03:22 +0600

    Update problem funciton name problemTestCases to testCases

--- app/Models/Problem.php
+++ app/Models/Problem.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Problem extends Model
 {
 
-  public function problemTestCases()
+  public function testCases()
   {
     return $this->hasMany(ProblemTestCase::class);
   }
