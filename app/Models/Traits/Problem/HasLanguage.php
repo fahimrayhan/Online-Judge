dcdc758f664a5429d75756f6d0d40a303c3e2793
amirhamza05 <sk.amirhamza@gmail.com>
2021-05-03 05:18:19 +0600

    Fix Model Traits Folder

---
+++ app/Models/Traits/Problem/HasLanguage.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models\Traits\Problem;
+
+trait HasLanguage
+{
+    /**
+     * Get all problems where problems are created or moderate(must accepted) by authenticate user
+     *
+     * @return boolean
+     */
+    public function hasLanguage($language_id)
+    {
+        $language = $this->languages()->where('language_id', $language_id)->first();
+        // dd($language->name);
+        if ($language) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
